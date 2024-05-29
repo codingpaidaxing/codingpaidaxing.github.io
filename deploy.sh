d976868b69d4885d2427aca7e1158dc24cfc7add
paidaxing <714771310@qq.com>
2024-05-29 17:02:04 +0800

    🐞 fix(init):

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 
-git push -f https://github.com/dromara/fast-request.git master:gh-pages
+git push -f https://github.com/codingpaidaxing/codingpaidaxing.github.io.git master:gh-pages
 
 cd -
